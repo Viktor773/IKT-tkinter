8c3f7927af7c604de54f22f872023beee01f9304
Viktor773 <viktor.varga2005@freemail.hu>
2022-03-22 11:13:55 +0100

    adatok bekérése

--- henger.py
+++ henger.py
@@ -1,5 +1,13 @@
 from tkinter import *
 foablak = Tk()
+Sugar = Label(foablak, text="Sugár")
+Sugar.pack()
+Sugarimport = Entry(foablak)
+Sugarimport.pack()
+Magassag=Label(foablak,text="Magasság")
+Magassag.pack()
+Magassagimport=Entry(foablak)
+Magassagimport.pack()
 
 
 foablak.mainloop()
