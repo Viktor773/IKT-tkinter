3a796bcb39de2523501e4e3184f725c3efe5d475
Viktor773 <viktor.varga2005@freemail.hu>
2022-03-29 11:00:31 +0200

    fajl kiterjesztesenek javitasa

---
+++ henger_folyadek.py
@@ -0,0 +1,19 @@
+from tkinter import *
+foablak = Tk()
+
+
+liter = Label(foablak,text="Liter")
+liter.pack()
+literimport = Entry(foablak)
+literimport.pack()
+magassag = Label(foablak, text="Magasság")
+magassag.pack()
+magassagimport = Entry(foablak)
+magassagimport.pack()
+sugar = Label(foablak,text="Sugár")
+sugar.pack()
+sugarimport=Entry(foablak)
+sugarimport.pack()
+
+
+foablak.mainloop()
